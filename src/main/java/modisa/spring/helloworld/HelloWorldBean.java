8811908f5d4dd422d46df3e0366a595823ddfa99
disah-code <disahcode@gmail.com>
2024-02-21 15:47:20 +0200

    Simple bean

---
+++ src/main/java/modisa/spring/helloworld/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package modisa.spring.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
