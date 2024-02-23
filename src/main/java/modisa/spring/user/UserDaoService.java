f2e8048abc0499c5c24f07076b81002e7499b80d
disah-code <disahcode@gmail.com>
2024-02-23 16:11:25 +0200

    retrive all users and single user

--- src/main/java/modisa/spring/user/UserDaoService.java
+++ src/main/java/modisa/spring/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -19,5 +20,10 @@ public class UserDaoService {
 
     public List<User> findAll() {
         return users;
+    }
+
+    public User findUser(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 }
